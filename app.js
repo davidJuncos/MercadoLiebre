57febab4e0920e9b416bf4c90b10d43018bba44e
unknown <davidjuncos@hotmail.com>
2022-03-16 00:11:26 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3000, () => {
+console.log("Servidor corriendo");
+});
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+});
+app.use(express.static('public'));
