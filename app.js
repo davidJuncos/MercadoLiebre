e5a6de3109c476a8302787cb4632083d5b486470
unknown <davidjuncos@hotmail.com>
2022-04-05 23:03:17 -0300

    cambios

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/',(req,res) => {
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 } )
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
 });
